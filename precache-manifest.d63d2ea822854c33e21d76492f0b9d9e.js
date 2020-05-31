ef7d6b1c9bf8f6ec71d5f571f9c378e2624c814d
Muskaanpal05 <palmuskaan5@gmail.com>
2020-05-31 19:09:55 +0530

    Updates

---
+++ precache-manifest.d63d2ea822854c33e21d76492f0b9d9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5461c6e020fa70805233e89ef2d2ebb",
+    "url": "/p-b-frontend/index.html"
+  },
+  {
+    "revision": "bee43c58839f4802e8bc",
+    "url": "/p-b-frontend/static/css/main.784ae677.chunk.css"
+  },
+  {
+    "revision": "70d048f383f2dbdeb6b6",
+    "url": "/p-b-frontend/static/js/2.928bcd0e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/p-b-frontend/static/js/2.928bcd0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bee43c58839f4802e8bc",
+    "url": "/p-b-frontend/static/js/main.1541e298.chunk.js"
+  },
+  {
+    "revision": "9338cd9aa86db8195791",
+    "url": "/p-b-frontend/static/js/runtime-main.e36936e4.js"
+  }
+]);
